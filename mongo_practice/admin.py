1f6bc6362f9b26db5cece0c8c496dbfdc7bbf96f
vedant711 <89075244+vedant711@users.noreply.github.com>
2023-04-17 18:11:59 +0530

    register-user-admin

--- mongo_practice/admin.py
+++ mongo_practice/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Transactions
+from .models import Transactions,CustomUser
 
 admin.site.register(Transactions)
+admin.site.register(CustomUser)
 
 # Register your models here.
